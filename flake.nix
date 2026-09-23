2102f4529b2cec539ca57c4e9b538e808ba48af8
Anjishnu Banerjee <kaezr.dev@gmail.com>
2026-09-23 13:52:59 +0530

    add nix flake and reduce firmware size

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
+  };
+
+  outputs =
+    inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          qmk
+          dos2unix
+          clang-tools
+        ];
+      };
+    };
+}
